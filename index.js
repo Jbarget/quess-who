61c59102b625e44e4c2e0601b4437bd5fb62409d
jbarget <dear.justen@gmail.com>
2019-03-02 12:52:00 +0100

    adds new people and fixes names

--- index.js
+++ index.js
@@ -1,137 +1,3 @@
-const people = {
-  'Adam Elmore': './images/Adam Elmore.jpg',
-  'Adam Park': './images/Adam Park.jpg',
-  'Alex Duncan': './images/Alex Duncan.jpg',
-  'Alice Davies': './images/Alice Davies.jpg',
-  'Andrew Harris': './images/Andrew Harris.jpg',
-  'Andy Element': './images/Andy Element.jpg',
-  'Anina Grostern': './images/Anina Grostern.jpg',
-  'Anusha Amin': './images/Anusha Amin.jpg',
-  'Asa Bradshaw': './images/Asa Bradshaw.jpg',
-  'Bobbi Talbot': './images/Bobbi Talbot.jpg',
-  'Bobby Steere': './images/Bobby Steere.jpg',
-  'Brooke Berte': './images/Brooke Berte.jpg',
-  'Callum Henderson': './images/Callum Henderson.jpg',
-  'Callum Taylor': './images/Callum Taylor.jpg',
-  'Cameron Stokes': './images/Cameron Stokes.jpg',
-  'Catherine Handley': './images/Catherine Handley.jpg',
-  'Cecily Melsom': './images/Cecily Melsom.jpg',
-  'Charlie Colwill': './images/Charlie Colwill.jpg',
-  'Charlie Greene': './images/Charlie Greene.jpg',
-  'Charlotte Emmersom': './images/Charlotte Emmersom.jpg',
-  'Charly Nelson': './images/Charly Nelson.jpg',
-  'Chris Priestly': './images/Chris Priestly.jpg',
-  'Christian Smith': './images/Christian Smith.jpg',
-  'Daisy Eakins': './images/Daisy Eakins.jpg',
-  'Dan Gillespie': './images/Dan Gillespie.jpg',
-  'Dayo Kamson': './images/Dayo Kamson.jpg',
-  'Dylan Davenport': './images/Dylan Davenport.jpg',
-  'Ed Burgess': './images/Ed Burgess.jpg',
-  'Eithne Bryan': './images/Eithne Bryan.jpg',
-  'Elliot Meller': './images/Elliot Meller.jpg',
-  'Elliya Cleveley': './images/Elliya Cleveley.jpg',
-  'Emily Barrett': './images/Emily Barrett.jpg',
-  'Emily Habin': './images/Emily Habin.jpg',
-  'Emma Baggott': './images/Emma Baggott.jpg',
-  'Emma Brazell': './images/Emma Brazell.jpg',
-  'Emma Davis': './images/Emma Davis.jpg',
-  'Emma Engelmann': './images/Emma Engelmann.jpg',
-  'Emma Guiness': './images/Emma Guiness.jpg',
-  'Eric Koeppel': './images/Eric Koeppel.jpg',
-  'Florence Beasley': './images/Florence Beasley.jpg',
-  'George MacDonald': './images/George MacDonald.jpg',
-  'George Pratt': './images/George Pratt.jpg',
-  'Gigi Wolujewicz': './images/Gigi Wolujewicz.jpg',
-  'Grace Potton': './images/Grace Potton.jpg',
-  'Greg Wickert': './images/Greg Wickert.jpg',
-  'Hannah Hawkins': './images/Hannah Hawkins.jpg',
-  'Hannah Watson': './images/Hannah Watson.jpg',
-  'Hannah Williams': './images/Hannah Williams.jpg',
-  'Harry Bamber': './images/Harry Bamber.jpg',
-  'Holly Tillbrook': './images/Holly Tillbrook.jpg',
-  'Hugh Woodward': './images/Hugh Woodward.jpg',
-  'Jack Godwin': './images/Jack Godwin.jpg',
-  'Jack Jeffery': './images/Jack Jeffery.jpg',
-  'James Evans': './images/James Evans.jpg',
-  'James McCaughley': './images/James McCaughley.jpg',
-  'Jamie Bolding': './images/Jamie Bolding.jpg',
-  'Jamie Slough': './images/Jamie Slough.jpg',
-  'Jesmira Bonoan': './images/Jesmira Bonoan.jpg',
-  'Jessica Gray': './images/Jessica Gray.jpg',
-  'Jodie Masser': './images/Jodie Masser.jpg',
-  'Joe Lewis': './images/Joe Lewis.jpg',
-  'Joe Terzeon': './images/Joe Terzeon.jpg',
-  'Jonathan Kitchen': './images/Jonathan Kitchen.jpg',
-  'Jordan Harvey': './images/Jordan Harvey.jpg',
-  'Jordan Paterson': './images/Jordan Paterson.jpg',
-  'Josh Kalms': './images/Josh Kalms.jpg',
-  'Kelsie Leonard': './images/Kelsie Leonard.jpg',
-  'Kim Holman': './images/Kim Holman.jpg',
-  'Laura Hutchinson': './images/Laura Hutchinson.jpg',
-  'Laura Smith': './images/Laura Smith.jpg',
-  'Laura Woodley': './images/Laura Woodley.jpg',
-  'Lauren Edwards': './images/Lauren Edwards.jpg',
-  'Lauren Kirkland': './images/Lauren Kirkland.jpg',
-  'Lewis Potton': './images/Lewis Potton.jpg',
-  'Liam Bedford': './images/Liam Bedford.jpg',
-  'Liam Chambers': './images/Liam Chambers.jpg',
-  'Lois Bellamy': './images/Lois Bellamy.jpg',
-  'Luke Hopkins': './images/Luke Hopkins.jpg',
-  'Marcus Gibson-Blair': './images/Marcus Gibson-Blair.jpg',
-  'Marcus Wilson': './images/Marcus Wilson.jpg',
-  'Marissa Giannone': './images/Marissa Giannone.jpg',
-  'Mark Spencer': './images/Mark Spencer.jpg',
-  'Matt Boner': './images/Matt Boner.jpg',
-  'Matt Hucks': './images/Matt Hucks.jpg',
-  'Melissa Chapman': './images/Melissa Chapman.jpg',
-  'Michelle Hambiliki': './images/Michelle Hambiliki.jpg',
-  'Mitch Strong': './images/Mitch Strong.jpg',
-  'Mitchell Armstrong': './images/Mitchell Armstrong.jpg',
-  'Molly Tappin': './images/Molly Tappin.jpg',
-  'Nadia Rahman': './images/Nadia Rahman.jpg',
-  'Nat Poulter': './images/Nat Poulter.jpg',
-  'Nathan Grace': './images/Nathan Grace.jpg',
-  'Nessa Humayun': './images/Nessa Humayun.jpg',
-  'Nina Edelshain': './images/Nina Edelshain.jpg',
-  'Oisin Bickley': './images/Oisin Bickley.jpg',
-  'Oli Warley': './images/Oli Warley.jpg',
-  'Oliver Ingham': './images/Oliver Ingham.jpg',
-  'Olivia DiTomaso': './images/Olivia DiTomaso.jpg',
-  'Olivia Podmore': './images/Olivia Podmore.jpg',
-  'Olivia Seaman': './images/Olivia Seaman.jpg',
-  'Olly Underwood': './images/Olly Underwood.jpg',
-  'Paul Beiboer': './images/Paul Beiboer.jpg',
-  'Polly Watson': './images/Polly Watson.jpg',
-  'Rachael Edwards': './images/Rachael Edwards.jpg',
-  'Richard Jones': './images/Richard Jones.jpg',
-  'Rob Katz': './images/Rob Katz.jpg',
-  'Rob Warren': './images/Rob Warren.jpg',
-  'Rory Metcalfe': './images/Rory Metcalfe.jpg',
-  'Rosie Box': './images/Rosie Box.jpg',
-  'Rosie Day': './images/Rosie Day.jpg',
-  'Ross Yellowlees': './images/Ross Yellowlees.jpg',
-  'Rozie Powell': './images/Rozie Powell.jpg',
-  'Sami Russell-Stracey': './images/Sami Russell-Stracey.jpg',
-  'Sarah Gyasi': './images/Sarah Gyasi.jpg',
-  'Sarah Peters': './images/Sarah Peters.jpg',
-  'Sarah Ricketts': './images/Sarah Ricketts.jpg',
-  'Siobhan Carty': './images/Siobhan Carty.jpg',
-  'Siobhan McDade': './images/Siobhan McDade.jpg',
-  'Sis Gurdal': './images/Sis Gurdal.jpg',
-  'Sophie Wyburd': './images/Sophie Wyburd.jpg',
-  'Stefan Armitage': './images/Stefan Armitage.jpg',
-  'Tanner Kelly': './images/Tanner Kelly.jpg',
-  'Tara Maini': './images/Tara Maini.jpg',
-  'Tawfiq Oladoja': './images/Tawfiq Oladoja.jpg',
-  'Thomas Lethbridge': './images/Thomas Lethbridge.jpg',
-  'Tom Jackson': './images/Tom Jackson.jpg',
-  'Tom McGirr': './images/Tom McGirr.jpg',
-  'Tyrone Brown': './images/Tyrone Brown.jpg',
-  'Will Hamnett': './images/Will Hamnett.jpg',
-  'William Turner': './images/William Turner.jpg',
-  'Yasmin Harding': './images/Yasmin Harding.jpg'
-}
-
 const getInitialState = (gameNames, numberOfRounds) => {
   const shuffledRounds = shuffleArray(gameNames)
   return {
@@ -199,33 +65,37 @@ const init = () => {
     .forEach(button => button.addEventListener('click', setNumberOfRounds))
 }
 
-const names = Object.keys(people)
-const numberOfNames = names.length
-
-const checkIndex = (indexToAvoid, randomIndexArray, generateIndex) => {
+const checkIndex = ({ nameToAvoid, namesBySex, randomIndexArray }) => {
+  const arrayLength = namesBySex.length
+  const generateIndex = () => Math.floor(Math.random() * arrayLength)
   const index = generateIndex()
-  if (index === indexToAvoid || randomIndexArray.includes(index)) {
-    return checkIndex(indexToAvoid, randomIndexArray, generateIndex)
+  if (namesBySex[index] === nameToAvoid || randomIndexArray.includes(index)) {
+    return checkIndex({ nameToAvoid, namesBySex, randomIndexArray })
   }
   return index
 }
 
-const generateRandomIndexArray = (indexToAvoid) => {
+const generateRandomIndexArray = ({ nameToAvoid, namesBySex }) => {
   let randomIndexArray = []
-  const generateIndex = () => Math.floor(Math.random() * numberOfNames)
   while (randomIndexArray.length < 3) {
-    const index = checkIndex(indexToAvoid, randomIndexArray, generateIndex)
+    const index = checkIndex({ nameToAvoid, namesBySex, randomIndexArray })
     randomIndexArray.push(index)
   }
 
   return randomIndexArray
 }
 
-const gameNames = names.map((name, i) => {
+const namesBySex = {
+  'M': people.filter((person, i) => person.sex === 'M'),
+  'F': people.filter((person, i) => person.sex === 'F')
+}
+
+const gameNames = people.map(({ name, image, sex }, i) => {
   return {
     realName: name,
-    image: people[name],
-    names: generateRandomIndexArray(i).map(index => names[index])
+    image: image,
+    names: generateRandomIndexArray({ nameToAvoid: name, namesBySex: namesBySex[sex] })
+      .map(index => namesBySex[sex][index].name)
   }
 })
 
---
+++ people.js
@@ -0,0 +1,583 @@
+const people = [{
+  image: './images/Abby Norelli.jpg',
+  sex: 'F',
+  name: 'Abby Norelli'
+}, {
+  image: './images/Adam Elmore.jpg',
+  sex: 'M',
+  name: 'Adam Elmore'
+}, {
+  name: 'Adam Park',
+  sex: 'M',
+  image: './images/Adam Park.jpg'
+}, {
+  name: 'Alex Duncan',
+  sex: 'M',
+  image: './images/Alex Duncan.jpg'
+}, {
+  name: 'Alice Davies',
+  sex: 'F',
+  image: './images/Alice Davies.jpg'
+}, {
+  name: 'Andrew Harris',
+  sex: 'M',
+  image: './images/Andrew Harris.jpg'
+}, {
+  name: 'Andy Element',
+  sex: 'M',
+  image: './images/Andy Element.jpg'
+}, {
+  name: 'Anina Grostern',
+  sex: 'F',
+  image: './images/Anina Grostern.jpg'
+}, {
+  name: 'Anusha Amin',
+  sex: 'F',
+  image: './images/Anusha Amin.jpg'
+}, {
+  name: 'Arianna Greenbaum',
+  sex: 'F',
+  image: './images/Arianna Greenbaum.jpg'
+}, {
+  name: 'Asa Bradshaw',
+  sex: 'M',
+  image: './images/Asa Bradshaw.jpg'
+}, {
+  name: 'Bobbi Talbot',
+  sex: 'F',
+  image: './images/Bobbi Talbot.jpg'
+}, {
+  name: 'Bobby Steere',
+  sex: 'M',
+  image: './images/Bobby Steere.jpg'
+}, {
+  name: 'Brooke Berte',
+  sex: 'F',
+  image: './images/Brooke Berte.jpg'
+}, {
+  name: 'Callum Henderson',
+  sex: 'M',
+  image: './images/Callum Henderson.jpg'
+}, {
+  name: 'Callum Taylor',
+  sex: 'M',
+  image: './images/Callum Taylor.jpg'
+}, {
+  name: 'Cameron Stokes',
+  sex: 'M',
+  image: './images/Cameron Stokes.jpg'
+}, {
+  name: 'Catherine Handley',
+  sex: 'F',
+  image: './images/Catherine Handley.jpg'
+}, {
+  name: 'Cecily Melsom',
+  sex: 'F',
+  image: './images/Cecily Melsom.jpg'
+}, {
+  name: 'Charlie Colwill',
+  sex: 'M',
+  image: './images/Charlie Colwill.jpg'
+}, {
+  name: 'Charlie Greene',
+  sex: 'M',
+  image: './images/Charlie Greene.jpg'
+}, {
+  name: 'Charlie Oshea',
+  sex: 'M',
+  image: './images/Charlie Oshea.jpg'
+}, {
+  name: 'Charlotte Emmersom',
+  sex: 'F',
+  image: './images/Charlotte Emmersom.jpg'
+}, {
+  name: 'Charly Nelson',
+  sex: 'F',
+  image: './images/Charly Nelson.jpg'
+}, {
+  name: 'Chris Hackett',
+  sex: 'M',
+  image: './images/Chris Hackett.jpg'
+}, {
+  name: 'Chris Priestly',
+  sex: 'M',
+  image: './images/Chris Priestly.jpg'
+}, {
+  name: 'Christian Smith',
+  sex: 'M',
+  image: './images/Christian Smith.jpg'
+}, {
+  name: 'Daisy Eakins',
+  sex: 'F',
+  image: './images/Daisy Eakins.jpg'
+}, {
+  name: 'Dan Gillespie',
+  sex: 'M',
+  image: './images/Dan Gillespie.jpg'
+}, {
+  name: 'Dayo Kamson',
+  sex: 'M',
+  image: './images/Dayo Kamson.jpg'
+}, {
+  name: 'Dylan Davenport',
+  sex: 'M',
+  image: './images/Dylan Davenport.jpg'
+}, {
+  name: 'Ed Burgess',
+  sex: 'M',
+  image: './images/Ed Burgess.jpg'
+}, {
+  name: 'Eithne Bryan',
+  sex: 'F',
+  image: './images/Eithne Bryan.jpg'
+}, {
+  name: 'Elliot Meller',
+  sex: 'M',
+  image: './images/Elliot Meller.jpg'
+}, {
+  name: 'Elliya Cleveley',
+  sex: 'F',
+  image: './images/Elliya Cleveley.jpg'
+}, {
+  name: 'Emily Barrett',
+  sex: 'F',
+  image: './images/Emily Barrett.jpg'
+}, {
+  name: 'Emily Habin',
+  sex: 'F',
+  image: './images/Emily Habin.jpg'
+}, {
+  name: 'Emma Baggott',
+  sex: 'F',
+  image: './images/Emma Baggott.jpg'
+}, {
+  name: 'Emma Brazell',
+  sex: 'F',
+  image: './images/Emma Brazell.jpg'
+}, {
+  name: 'Emma Davis',
+  sex: 'F',
+  image: './images/Emma Davis.jpg'
+}, {
+  name: 'Emma Engelmann',
+  sex: 'F',
+  image: './images/Emma Engelmann.jpg'
+}, {
+  name: 'Emma Guiness',
+  sex: 'F',
+  image: './images/Emma Guiness.jpg'
+}, {
+  name: 'Eric Koeppel',
+  sex: 'M',
+  image: './images/Eric Koeppel.jpg'
+}, {
+  name: 'Florence Beasley',
+  sex: 'F',
+  image: './images/Florence Beasley.jpg'
+}, {
+  name: 'George MacDonald',
+  sex: 'M',
+  image: './images/George MacDonald.jpg'
+}, {
+  name: 'George Pratt',
+  sex: 'M',
+  image: './images/George Pratt.jpg'
+}, {
+  name: 'Gigi Wolujewicz',
+  sex: 'F',
+  image: './images/Gigi Wolujewicz.jpg'
+}, {
+  name: 'Grace Potton',
+  sex: 'F',
+  image: './images/Grace Potton.jpg'
+}, {
+  name: 'Greg Wickert',
+  sex: 'M',
+  image: './images/Greg Wickert.jpg'
+}, {
+  name: 'Hannah Hawkins',
+  sex: 'F',
+  image: './images/Hannah Hawkins.jpg'
+}, {
+  name: 'Hannah Loxley',
+  sex: 'F',
+  image: './images/Hannah Loxley.jpg'
+}, {
+  name: 'Hannah Watson',
+  sex: 'F',
+  image: './images/Hannah Watson.jpg'
+}, {
+  name: 'Hannah Williams',
+  sex: 'F',
+  image: './images/Hannah Williams.jpg'
+}, {
+  name: 'Harry Bamber',
+  sex: 'M',
+  image: './images/Harry Bamber.jpg'
+}, {
+  name: 'Henry Smith',
+  sex: 'M',
+  image: './images/Henry Smith.jpg'
+}, {
+  name: 'Holly Tillbrook',
+  sex: 'F',
+  image: './images/Holly Tillbrook.jpg'
+}, {
+  name: 'Hugh Woodward',
+  sex: 'M',
+  image: './images/Hugh Woodward.jpg'
+}, {
+  name: 'Jack Godwin',
+  sex: 'M',
+  image: './images/Jack Godwin.jpg'
+}, {
+  name: 'Jack Jeffery',
+  sex: 'M',
+  image: './images/Jack Jeffery.jpg'
+}, {
+  name: 'James Evans',
+  sex: 'M',
+  image: './images/James Evans.jpg'
+}, {
+  name: 'James McCaughley',
+  sex: 'M',
+  image: './images/James McCaughley.jpg'
+}, {
+  name: 'Jamie Bolding',
+  sex: 'M',
+  image: './images/Jamie Bolding.jpg'
+}, {
+  name: 'Jamie Fallon',
+  sex: 'M',
+  image: './images/Jamie Fallon.jpg'
+}, {
+  name: 'Jamie Slough',
+  sex: 'M',
+  image: './images/Jamie Slough.jpg'
+}, {
+  name: 'Jesmira Bonoan',
+  sex: 'F',
+  image: './images/Jesmira Bonoan.jpg'
+}, {
+  name: 'Jessica Gray',
+  sex: 'F',
+  image: './images/Jessica Gray.jpg'
+}, {
+  name: 'Jodie Masser',
+  sex: 'F',
+  image: './images/Jodie Masser.jpg'
+}, {
+  name: 'Joe Dunn',
+  sex: 'M',
+  image: './images/Joe Dunn.jpg'
+}, {
+  name: 'Joe Lewis',
+  sex: 'M',
+  image: './images/Joe Lewis.jpg'
+}, {
+  name: 'Joe Terzeon',
+  sex: 'M',
+  image: './images/Joe Terzeon.jpg'
+}, {
+  name: 'Jonathan Kitchen',
+  sex: 'M',
+  image: './images/Jonathan Kitchen.jpg'
+}, {
+  name: 'Jordan Harvey',
+  sex: 'F',
+  image: './images/Jordan Harvey.jpg'
+}, {
+  name: 'Jordan Paterson',
+  sex: 'M',
+  image: './images/Jordan Paterson.jpg'
+}, {
+  name: 'Josh Kalms',
+  sex: 'M',
+  image: './images/Josh Kalms.jpg'
+}, {
+  name: 'Kelsie Leonard',
+  sex: 'F',
+  image: './images/Kelsie Leonard.jpg'
+}, {
+  name: 'Kim Holman',
+  sex: 'M',
+  image: './images/Kim Holman.jpg'
+}, {
+  name: 'Laura Hutchinson',
+  sex: 'F',
+  image: './images/Laura Hutchinson.jpg'
+}, {
+  name: 'Laura Smith',
+  sex: 'F',
+  image: './images/Laura Smith.jpg'
+}, {
+  name: 'Laura Woodley',
+  sex: 'F',
+  image: './images/Laura Woodley.jpg'
+}, {
+  name: 'Lauren Edwards',
+  sex: 'F',
+  image: './images/Lauren Edwards.jpg'
+}, {
+  name: 'Lauren Kirkland',
+  sex: 'F',
+  image: './images/Lauren Kirkland.jpg'
+}, {
+  name: 'Lewis Potton',
+  sex: 'M',
+  image: './images/Lewis Potton.jpg'
+}, {
+  name: 'Liam Bedford',
+  sex: 'M',
+  image: './images/Liam Bedford.jpg'
+}, {
+  name: 'Liam Chambers',
+  sex: 'M',
+  image: './images/Liam Chambers.jpg'
+}, {
+  name: 'Lily Hanson',
+  sex: 'F',
+  image: './images/Lily Hanson.jpg'
+}, {
+  name: 'Lois Bellamy',
+  sex: 'F',
+  image: './images/Lois Bellamy.jpg'
+}, {
+  name: 'Luke Hopkins',
+  sex: 'M',
+  image: './images/Luke Hopkins.jpg'
+}, {
+  name: 'Marcus Gibson-Blair',
+  sex: 'M',
+  image: './images/Marcus Gibson-Blair.jpg'
+}, {
+  name: 'Marcus Wilson',
+  sex: 'M',
+  image: './images/Marcus Wilson.jpg'
+}, {
+  name: 'Marissa Giannone',
+  sex: 'F',
+  image: './images/Marissa Giannone.jpg'
+}, {
+  name: 'Mark Spencer',
+  sex: 'M',
+  image: './images/Mark Spencer.jpg'
+}, {
+  name: 'Matt Boner',
+  sex: 'M',
+  image: './images/Matt Boner.jpg'
+}, {
+  name: 'Matt Hucks',
+  sex: 'M',
+  image: './images/Matt Hucks.jpg'
+}, {
+  name: 'Melissa Chapman',
+  sex: 'F',
+  image: './images/Melissa Chapman.jpg'
+}, {
+  name: 'Michelle Hambiliki',
+  sex: 'F',
+  image: './images/Michelle Hambiliki.jpg'
+}, {
+  name: 'Mitch Strong',
+  sex: 'M',
+  image: './images/Mitch Strong.jpg'
+}, {
+  name: 'Mitchell Armstrong',
+  sex: 'M',
+  image: './images/Mitchell Armstrong.jpg'
+}, {
+  name: 'Molly Tappin',
+  sex: 'F',
+  image: './images/Molly Tappin.jpg'
+}, {
+  name: 'Nadia Rahman',
+  sex: 'F',
+  image: './images/Nadia Rahman.jpg'
+}, {
+  name: 'Nat Poulter',
+  sex: 'M',
+  image: './images/Nat Poulter.jpg'
+}, {
+  name: 'Nathan Grace',
+  sex: 'M',
+  image: './images/Nathan Grace.jpg'
+}, {
+  name: 'Nessa Humayun',
+  sex: 'F',
+  image: './images/Nessa Humayun.jpg'
+}, {
+  name: 'Nina Edelshain',
+  sex: 'F',
+  image: './images/Nina Edelshain.jpg'
+}, {
+  name: 'Oisin Bickley',
+  sex: 'M',
+  image: './images/Oisin Bickley.jpg'
+}, {
+  name: 'Oli Warley',
+  sex: 'M',
+  image: './images/Oli Warley.jpg'
+}, {
+  name: 'Oliver Ingham',
+  sex: 'M',
+  image: './images/Oliver Ingham.jpg'
+}, {
+  name: 'Olivia DiTomaso',
+  sex: 'F',
+  image: './images/Olivia DiTomaso.jpg'
+}, {
+  name: 'Olivia Podmore',
+  sex: 'F',
+  image: './images/Olivia Podmore.jpg'
+}, {
+  name: 'Olivia Seaman',
+  sex: 'F',
+  image: './images/Olivia Seaman.jpg'
+}, {
+  name: 'Olly Underwood',
+  sex: 'M',
+  image: './images/Olly Underwood.jpg'
+}, {
+  name: 'Paul Beiboer',
+  sex: 'M',
+  image: './images/Paul Beiboer.jpg'
+}, {
+  name: 'Polly Watson',
+  sex: 'F',
+  image: './images/Polly Watson.jpg'
+}, {
+  name: 'Rachael Edwards',
+  sex: 'F',
+  image: './images/Rachael Edwards.jpg'
+}, {
+  name: 'Richard Jones',
+  sex: 'M',
+  image: './images/Richard Jones.jpg'
+}, {
+  name: 'Rob Katz',
+  sex: 'M',
+  image: './images/Rob Katz.jpg'
+}, {
+  name: 'Rob Warren',
+  sex: 'M',
+  image: './images/Rob Warren.jpg'
+}, {
+  name: 'Rory Metcalfe',
+  sex: 'M',
+  image: './images/Rory Metcalfe.jpg'
+}, {
+  name: 'Rosie Box',
+  sex: 'F',
+  image: './images/Rosie Box.jpg'
+}, {
+  name: 'Rosie Day',
+  sex: 'F',
+  image: './images/Rosie Day.jpg'
+}, {
+  name: 'Ross Yellowlees',
+  sex: 'M',
+  image: './images/Ross Yellowlees.jpg'
+}, {
+  name: 'Rozie Powell',
+  sex: 'F',
+  image: './images/Rozie Powell.jpg'
+}, {
+  name: 'Sami Russell-Stracey',
+  sex: 'F',
+  image: './images/Sami Russell-Stracey.jpg'
+}, {
+  name: 'Sarah Gyasi',
+  sex: 'F',
+  image: './images/Sarah Gyasi.jpg'
+}, {
+  name: 'Sarah Peters',
+  sex: 'F',
+  image: './images/Sarah Peters.jpg'
+}, {
+  name: 'Sarah Ricketts',
+  sex: 'F',
+  image: './images/Sarah Ricketts.jpg'
+}, {
+  name: 'Sean Longstaff',
+  sex: 'M',
+  image: './images/Sean Longstaff.jpg'
+}, {
+  name: 'Sharon Budwal',
+  sex: 'F',
+  image: './images/Sharon Budwal.jpg'
+}, {
+  name: 'Siobhan Carty',
+  sex: 'F',
+  image: './images/Siobhan Carty.jpg'
+}, {
+  name: 'Siobhan McDade',
+  sex: 'F',
+  image: './images/Siobhan McDade.jpg'
+}, {
+  name: 'Sis Gurdal',
+  sex: 'F',
+  image: './images/Sis Gurdal.jpg'
+}, {
+  name: 'Sophie Wyburd',
+  sex: 'F',
+  image: './images/Sophie Wyburd.jpg'
+}, {
+  name: 'Stefan Armitage',
+  sex: 'M',
+  image: './images/Stefan Armitage.png'
+}, {
+  name: 'Tacita Brown',
+  sex: 'F',
+  image: './images/Tacita Brown.jpg'
+}, {
+  name: 'Tanner Kelly',
+  sex: 'M',
+  image: './images/Tanner Kelly.jpg'
+}, {
+  name: 'Tara Maini',
+  sex: 'F',
+  image: './images/Tara Maini.jpg'
+}, {
+  name: 'Tawfiq Oladoja',
+  sex: 'M',
+  image: './images/Tawfiq Oladoja.jpg'
+}, {
+  name: 'Thomas Lethbridge',
+  sex: 'M',
+  image: './images/Thomas Lethbridge.jpg'
+}, {
+  name: 'Tom Jackson',
+  sex: 'M',
+  image: './images/Tom Jackson.jpg'
+}, {
+  name: 'Tom Longstaff',
+  sex: 'M',
+  image: './images/Tom Longstaff.jpg'
+}, {
+  name: 'Tom McGirr',
+  sex: 'M',
+  image: './images/Tom McGirr.jpg'
+}, {
+  name: 'Tyrone Brown',
+  sex: 'M',
+  image: './images/Tyrone Brown.jpg'
+}, {
+  name: 'Will Hamnett',
+  sex: 'M',
+  image: './images/Will Hamnett.jpg'
+}, {
+  name: 'William Turner',
+  sex: 'M',
+  image: './images/William Turner.jpg'
+}, {
+  name: 'Yasmin Harding',
+  sex: 'F',
+  image: './images/Yasmin Harding.jpg'
+}, {
+  name: 'Zach Marsden',
+  sex: 'M',
+  image: './images/Zach Marsden.jpg'
+}]
+
+window.people = people
